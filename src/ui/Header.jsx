9b94b727d088bea368e5dd39bccb2e3325b8b570
10urkarasu <10urkarasu@gmail.com>
2024-03-27 23:16:35 +0300

    first commit

---
+++ src/ui/Header.jsx
@@ -0,0 +1,13 @@
+import { Link } from "react-router-dom";
+
+function Header() {
+    return (
+        <header>
+            <Link to="/">Pizza Company</Link>
+
+            <p>Onur</p>
+        </header>
+    );
+}
+
+export default Header;
